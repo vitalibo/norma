8bcebd8ba758a96bca568013db2ef4b042d7c454
Vitaliy Boyarsky <vitaliy.boyarsky@sigma.software>
2026-07-07 04:25:54 +0300

    Covered nested get/set/drop expr by tests

--- tests/unit/norma/engines/pyspark/test_utils.py
+++ tests/unit/norma/engines/pyspark/test_utils.py
@@ -1,9 +1,12 @@
 import pytest
+from pyspark.sql import Row
 from pyspark.sql import functions as fn
 from pyspark.sql.types import ArrayType, IntegerType, StringType, StructField, StructType
 from pyxis import resources
 
-from norma.engines.pyspark.utils import dtype_at, dtype_drop, dtype_set, nested_set_expr
+from norma.engines.pyspark.utils import (
+    dtype_at, dtype_drop, dtype_set, nested_drop_expr, nested_get_expr, nested_set_expr
+)
 
 
 @pytest.mark.parametrize('case, column', [
@@ -22,18 +25,18 @@ from norma.engines.pyspark.utils import dtype_at, dtype_drop, dtype_set, nested_
 def test_nested_set_expr(case, column, spark):
     df = spark.create_dataframe_from_resource(
         __file__,
-        f'data/with_nested_column/{case}/in.json',
-        f'data/with_nested_column/{case}/in_schema.json'
+        f'data/nested_set_expr/{case}/in.json',
+        f'data/nested_set_expr/{case}/in_schema.json'
     )
     expected = spark.create_dataframe_from_resource(
         __file__,
-        f'data/with_nested_column/{case}/exp.json',
-        f'data/with_nested_column/{case}/exp_schema.json'
+        f'data/nested_set_expr/{case}/exp.json',
+        f'data/nested_set_expr/{case}/exp_schema.json'
     )
     schema = StructType.from_json(  # pylint:disable=no-member
         resources.resource_as_json(
             __file__,
-            f'data/with_nested_column/{case}/exp_schema.json'
+            f'data/nested_set_expr/{case}/exp_schema.json'
         )
     )
 
@@ -44,6 +47,64 @@ def test_nested_set_expr(case, column, spark):
         fn.col(root) if exists else None,
         df.schema[root].dataType if exists else None
     ))
+
+    spark.assert_dataframe_equals(
+        actual, expected, schema=schema)
+
+
+@pytest.mark.parametrize('column, root_expr, expected', [
+    pytest.param(column, args[0] if args else lambda: None, expected,
+                 id=f'Case #{i}: {name}') for i, (name, column, expected, *args) in enumerate([
+        ('top-level', 'f1', ['v1', 'v2']),
+        ('nested field', 'f2.f3.f4', ['a', None]),
+        ('array column itself', 'f5[]', [[Row(f6='x1', f7=Row(f8='y1')), Row(f6='x2', f7=Row(f8='y2'))], None]),
+        ('field of array elements', 'f5[].f6', [['x1', 'x2'], None]),
+        ('deeper field of array elements', 'f5[].f7.f8', [['y1', 'y2'], None]),
+        ('use root_expr instead of fn.col of the root', 'f2.f3.f4', ['A', None], lambda: fn.col('f2').withField(
+            'f3', fn.col('f2').getField('f3').withField('f4', fn.upper(fn.col('f2').getField('f3').getField('f4'))))),
+    ])
+])
+def test_nested_get_expr(column, root_expr, expected, spark):
+    df = spark.create_dataframe_from_resource(
+        __file__,
+        'data/nested_get_expr/in.json',
+        'data/nested_get_expr/in_schema.json'
+    )
+
+    rows = df.select(nested_get_expr(column, root_expr()).alias('out')).collect()
+    assert [row['out'] for row in rows] == expected
+
+
+@pytest.mark.parametrize('case, column', [
+    pytest.param(f'case{i}', column, id=f'Case #{i}: {name}') for i, (name, column) in enumerate([
+        ('drop nested field', 'f1.f2'),
+        ('drop deeply nested field', 'f1.f2.f3'),
+        ('drop field of array element structs', 'f1.f2[].f4'),
+        ('drop nested field inside array element structs', 'f1.f2[].f3.f5'),
+        ('drop field with array root', 'f1[].f2'),
+    ])
+])
+def test_nested_drop_expr(case, column, spark):
+    df = spark.create_dataframe_from_resource(
+        __file__,
+        f'data/nested_drop_expr/{case}/in.json',
+        f'data/nested_drop_expr/{case}/in_schema.json'
+    )
+    expected = spark.create_dataframe_from_resource(
+        __file__,
+        f'data/nested_drop_expr/{case}/exp.json',
+        f'data/nested_drop_expr/{case}/exp_schema.json'
+    )
+    schema = StructType.from_json(  # pylint:disable=no-member
+        resources.resource_as_json(
+            __file__,
+            f'data/nested_drop_expr/{case}/exp_schema.json'
+        )
+    )
+
+    root = column.split('.')[0].removesuffix('[]')
+    actual = df.withColumn(root, nested_drop_expr(
+        column, fn.col(root), df.schema[root].dataType))
 
     spark.assert_dataframe_equals(
         actual, expected, schema=schema)
